71646e60b16a3e2ffebee19de4eb39f38da9506a
Luckie Bah <bah966@gmail.com>
2023-03-20 13:14:04 +0000

    Organized made notes and fixed corrections

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,37 +5,33 @@ console.log('****** Part Supply *******');
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
 let partNeeded = 40;
-console.log(partNeeded);
+
+
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 let supplyChanges = [3, 5, -6, 0, 7, 11]
-console.log(supplyChanges);
-// 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
-console.log(supplyChanges[1]);
+
+
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 let removedItem = supplyChanges.pop()
-console.log(removedItem)
+
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 supplyChanges.push(25)
-console.log(supplyChanges);
+
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 
-
-
 for (let x = 0; x < supplyChanges.length; x++) {
     console.log(supplyChanges[x]);
     if (supplyChanges[x] > 0) {
         console.log("Added x parts.");
-        ;
     }
     if (supplyChanges[x] === 0) {
         console.log("No Change");
@@ -45,13 +41,13 @@ for (let x = 0; x < supplyChanges.length; x++) {
     }
 }
 
-
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
 let x = 0
-for (each of supplyChanges) { console.log(supplyChanges[x]); x++ };
+for (each of supplyChanges) { 
+    console.log(supplyChanges[x]); x++ };
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
@@ -59,9 +55,8 @@ console.log('8. Total supplies available is:');
 
 let sum = 0;
 for (let x of supplyChanges) {
-     sum = sum + x; 
+    sum = sum + x;
 };
-    console.log(sum);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -70,26 +65,45 @@ for (let x of supplyChanges) {
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
-// Declared parts and total
 let partsTotal = 572;
-let total = 0;
-// While statement stating to add 7 until total was 
-// greater then 572 and then to log it.
-while (partsTotal > total) { 
-       total = total + 7 
-};
-      console.log(total);
-// Made a if statement to check if total had become 
-// higher then 572 if so to subtract 7 so that the 
-// correct number of boxes would be stated
-if (total > partsTotal) { total = total - 7 };
-// declared excess and boxesFilled by subtracting
-// partsTotal variable from the total variable and
-// dividing the total variable by 7.
+let maxBoxes = 7
+let zero = []
+while (partsTotal >= maxBoxes) {
+    zero+=7
+    if (partsTotal >= maxBoxes) {
+        partsTotal -= maxBoxes
+    }
+}
 
-let excess = partsTotal - total;
-let boxesFilled = total / 7;
+//CONSOLE LOGS-----------------------------------------------------------
 
-console.log('Their are',  boxesFilled,'boxes filled' , 
-            'And', excess ,'parts remain'
-);
+// Used for the partNeeded variable to log the number 
+// of parts needed
+console.log(partNeeded);
+
+// Used for the supply changes variable to log the numbers
+// in the supply 
+console.log(supplyChanges);
+
+// 3. Console log the value of the second item in the 'supplyChanges' array
+console.log('3. Second supplyChange is:');
+console.log(supplyChanges[1]);
+
+// Used to log the removed item from the removedItem variable
+// which takes in the pop command which removes the last item
+console.log(removedItem)
+
+// Used to log the pushed number 25 into the variable
+// supply changes
+console.log(supplyChanges);
+
+// Used for the sum varibale to display the sum of 
+// the supplyChanges variable
+console.log(sum);
+
+// Used to log the number of boxes filled and excess from 
+// the partsTotal variable
+console.log('this is the excess', partsTotal)
+console.log('this is the boxes filled',zero.length);
+
+
